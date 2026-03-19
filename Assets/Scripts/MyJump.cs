dacc35c5c351ef6bc0deacce08cbbc2ebca2393d
yoojoonlee0424 <yoojoonlee0424@gmail.com>
2026-03-19 12:05:00 +0900

    [Feat] 유니티 컴포넌트 기초 제작, Timer 코딩, Input

---
+++ Assets/Scripts/MyJump.cs
@@ -0,0 +1,39 @@
+using NUnit.Framework.Internal;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyJump : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    public float power = 200f;
+    public Text timeUI;
+    public float Timer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Timer = Timer + Time.deltaTime;
+        timeUI.text = Timer.ToString();
+
+
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            power = power * Random.Range(-1,2);
+            rigidbody.AddForce(transform.up * power);
+        }
+
+        if (this.gameObject.transform.position.y > 50 || this.gameObject.transform.position.y < -30)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Mycharacter.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Mycharacter : MonoBehaviour
+{
+    public int Health = 100;
+    public float Timer = 1.0f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Health = Health + 100;                                                  // 시작시 체력추가
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer = Timer - Time.deltaTime;                                     //매 프레임 시간 감소
+
+        if (Timer <= 0)                                                     //만약 Timer 의 수치가 0이하로 내려갈 경우
+        {
+            Timer = 1.0f;                                                   // 다시 1초로 변경
+            Health = Health - 20;                                           //처력 감소
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            Health = Health + Random.Range(-50,100);
+        }
+
+        if (Health <= 0)
+        { 
+            Destroy(this.gameObject);
+        }
+    }
+}
